a9567225851020219c9788cb5b13cf1c23f56d58
Hitagi <include.ofstream.h@gmail.com>
2021-04-27 17:25:48 +0300

    first commit

---
+++ ConfigurationApp/Startup.cs
@@ -0,0 +1,90 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConfigurationApp
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public static class SessionExtensions
+    {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonSerializer.Serialize<T>(value));
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonSerializer.Deserialize<T>(value);
+        }
+    }
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder().AddConfiguration(configuration);
+            AppConfiguration = builder.Build();
+        }
+
+        // свойство, которое будет хранить конфигурацию
+        public IConfiguration AppConfiguration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSession();
+            app.Run(async (context) =>
+            {
+                if (context.Session.Keys.Contains("person"))
+                {
+                    Person person = context.Session.Get<Person>("person");
+                    await context.Response.WriteAsync($"Hello {person.Name}, your age: {person.Age}!");
+                }
+                else
+                {
+                    Person person = new Person { Name = "Tom", Age = 22 };
+                    context.Session.Set<Person>("person", person);
+                    await context.Response.WriteAsync("Hello World!");
+                }
+            });
+        }
+        private string GetSectionContent(IConfiguration configSection)
+        {
+            string sectionContent = "";
+            foreach (var section in configSection.GetChildren())
+            {
+                sectionContent += "\"" + section.Key + "\":";
+                if (section.Value == null)
+                {
+                    string subSectionContent = GetSectionContent(section);
+                    sectionContent += "{\n" + subSectionContent + "},\n";
+                }
+                else
+                {
+                    sectionContent += "\"" + section.Value + "\",\n";
+                }
+            }
+            return sectionContent;
+        }
+    }
+}
